f1d100122f8b86d6484459786a3e2460061b4c29
sari <semih.sari@windowslive.com>
2024-09-02 16:24:38 +0300

    Add Signal Handling for Automatic Profile Creation on User Registration
    - Implemented a Django signal to automatically create a `Profile` instance when a new `CustomUser` is created.
    - Added a `create_user_profile` function that listens to the `post_save` signal of the `CustomUser` model.
    - Ensured that a `Profile` is created for each new user upon registration, linking it to the corresponding `CustomUser` instance.
    - Updated `profiles` app to include signal handling and associated profile creation logic.
    
    This enhancement streamlines the user onboarding process by ensuring that each user has an associated profile created automatically.

--- profiles/apps.py
+++ profiles/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "profiles"
+
+    def read(self):
+        from profiles import signals
