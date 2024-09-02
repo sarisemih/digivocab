63c27a1882bed90d038d1eb66c1f7327da8590bc
sari <semih.sari@windowslive.com>
2024-09-03 00:07:36 +0300

    Add vocabularies app with initial models

---
+++ vocabularies/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+from profiles.models import Profile
+
+class Vocabulary(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='vocabularies')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.profile.user.username})"
+    
+
+class Word(models.Model):
+    dictionary=models.ForeignKey(Vocabulary, on_delete=models.CASCADE, related_name='words')
+    text = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    example_sentence = models.TextField()
+
+    def __str__(self):
+        return self.text
